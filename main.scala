64a084a2ecf63842e4fa9ecffe63b7adb9205cb2
plrnvld <paulrijneveld@gmail.com>
2021-12-07 22:05:27 +0000

    Finish part 1 & 2

--- main.scala
+++ main.scala
@@ -2,13 +2,31 @@ import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val fish = Source.fromFile("Example.txt").getLines.next.split(",").map(t => t.toInt).toList
+    val fish = Source.fromFile("Input.txt").getLines.next.split(",").map(t => t.toInt).toList
+    val fishCount = countFishDays(fish)
 
-    (1 to 18).foldLeft(fish) { iterateDay }
+    println(s"It's day 0, fishes: ${fishCount.total}")
+
+    (1 to 256).foldLeft(fishCount)((f, d) => f.addNewFishes().iterateDay(d))
   }
 
-  def iterateDay(fish: List[Int], day: Int): List[Int] = {
-      println(s"It's day $day, fish: $fish")
-      fish
+  def countFishDays(fish: List[Int]): FishDaysCount = {
+    FishDaysCount(fish.filter(_ == 0).length, fish.filter(_ == 1).length, fish.filter(_ == 2).length, fish.filter(_ == 3).length,fish.filter(_ == 4).length, fish.filter(_ == 5).length, fish.filter(_ == 6).length, fish.filter(_ == 7).length, fish.filter(_ == 8).length, fish.filter(_ == 9).length)
+  }
+
+  case class FishDaysCount(zero: Long, one: Long, two: Long, three: Long, four: Long, five: Long, six: Long, seven: Long, eight: Long, nine: Long) {
+    def total = zero + one + two + three + four + five + six + seven + eight + nine
+
+    def addNewFishes(): FishDaysCount = {
+      FishDaysCount(zero, one, two, three, four, five, six, seven, eight, zero)
+    }
+
+    def iterateDay(day: Int): FishDaysCount = {
+      val newCount = FishDaysCount(one, two, three, four, five, six, seven + zero, eight, nine, 0)
+
+      println(s"It's day $day, fishes: ${newCount.total}")
+
+      newCount
+    }
   }
 }
